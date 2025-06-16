8efc1de8b6cf6c5c1f08931dd2c6f2213e6f4253
Ryuki0530 <159127441+Ryuki0530@users.noreply.github.com>
2025-06-17 01:10:28 +0900

    完成と反省点の追加

--- 20250617/main.c
+++ 20250617/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#define INPUT_STR_LEN 1000
 
 int main() {
-    printf("Hello, World!\n");
+    char str[INPUT_STR_LEN];
+    char prev_char = '\0';
+    int count = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    // ランレングス圧縮
+    for(int i = 0; str[i] != '\0'; i++) {
+        char current_char = str[i];
+        if(current_char == '\n') break;
+
+        if(prev_char == '\0') {
+            prev_char = current_char;
+            count = 1;
+        } else if(prev_char == current_char) {
+            count++;
+        } else {
+            printf("%c%d", prev_char, count);
+            prev_char = current_char;
+            count = 1;
+        }
+    }
+    // 最後の文字の処理
+    if(prev_char != '\0' ) {
+        printf("%c%d", prev_char, count);
+    }
+
+    printf("\n");
+    //debug
+    // printf("入力文字列: %s\n", str);
     return 0;
 }
