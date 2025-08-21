214ac8375ee91e89a401fdfde732629e0f9c8660
Ryuki <ryukifujita0530@gmail.com>
2025-08-21 23:23:06 +0900

    完成

--- 2025/08/20250821/main.c
+++ 2025/08/20250821/main.c
@@ -1,6 +1,39 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+
+bool is_uppercase(char c) {
+    return c >= 'A' && c <= 'Z';
+}
+
+bool is_lowercase(char c) {
+    return c >= 'a' && c <= 'z';
+}
+
+char to_lowercase(char c) {
+    return c + ('a' - 'A');
+}
+char to_uppercase(char c) {
+    return c - ('a' - 'A');
+}
+
+char *str_toggle_case(char *str) {
+    for(int i = 0; str[i]; i++) {
+        if(is_uppercase(str[i])) {
+            str[i] = to_lowercase(str[i]);
+        } else if(is_lowercase(str[i])) {
+            str[i] = to_uppercase(str[i]);
+        } else {
+            // Non-alphabetic characters remain unchanged   
+        }
+    }
+    return str;
+}
 
 int main() {
-    printf("Hello World 123\n");
+    char str[100];
+    fgets(str, sizeof(str), stdin);
+    str_toggle_case(str);
+    printf("%s", str);
     return 0;
 }
