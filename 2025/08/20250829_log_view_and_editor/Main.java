22d22fcd5205589fb7835555e711d6a67ba5148b
Ryuki <ryukifujita0530@gmail.com>
2025-08-29 04:51:53 +0900

    GUIベース作成

---
+++ 2025/08/20250829_log_view_and_editor/Main.java
@@ -0,0 +1,7 @@
+import view.LogFrame;
+public class Main {
+    public static void main(String[] args) {
+        LogFrame logFrame = new LogFrame();
+        logFrame.setVisible(true);
+    }
+}
---
+++ 2025/08/20250829_log_view_and_editor/view/LogFrame.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.JFrame;
+public class LogFrame extends JFrame {
+    public LogFrame() {
+        super("ログビューア");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+    }
+}
