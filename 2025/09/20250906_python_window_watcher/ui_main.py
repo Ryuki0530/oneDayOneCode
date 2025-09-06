e304213026ee3b35220a7ce2e1e85ac91fabdbbd
Ryuki <ryukifujita0530@gmail.com>
2025-09-06 23:12:55 +0900

    取得したウィンドウ一覧を表示するアプリケーションのコードを追加

--- 2025/09/20250906_python_window_watcher/ui_main.py
+++ 2025/09/20250906_python_window_watcher/ui_main.py
@@ -2,12 +2,15 @@ import sys
 import argparse
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QListWidget, QPushButton
 from PyQt6.QtCore import QTimer, QDateTime
+from winenum import WinEnumService
 
 class MainWindow(QWidget):
     def __init__(self, args):
 
         super().__init__()
+        self.args = args
         self.setWindowTitle("window watcher")
+        self.setFixedSize(900, 600)
         self.status_label = QLabel("状態: 初期化中")
         self.log_list = QListWidget()
         self.start_btn = QPushButton("開始")
@@ -28,6 +31,8 @@ class MainWindow(QWidget):
         self.start_btn.clicked.connect(self.start_detection)
         self.stop_btn.clicked.connect(self.stop_detection)
 
+        self.WinEnumService = WinEnumService()
+
         self.status_label.setText("状態: 待機中")
 
     def start_detection(self):
@@ -47,10 +52,24 @@ class MainWindow(QWidget):
         self.log_list.addItem(message)
         self.log_list.scrollToBottom()
 
+    def init_log(self):
+        self.log_list.clear()
+
     def check_window(self):
         now = QDateTime.currentDateTime().toString("yyyy-MM-dd HH:mm:ss")
-        self.add_log(f"[{now}] Checking window...")
-        # ここにウィンドウ監視のロジックを追加
+
+        include_empty = self.args.include_empty
+        all_styles = self.args.all_styles
+
+        windows = self.WinEnumService.enumerate(include_empty=include_empty, all_styles=all_styles, include_exe=False)
+        
+        self.init_log()
+        self.add_log(f"[{now}] Checked windows.")
+
+        for window in windows:
+            self.add_log(f"name: {window.title} pid: {window.pid} class: {window.class_name} fg: {window.is_foreground}")
+        self.add_log(f"==================\nTotal windows: {len(windows)}")
+
 
     def closeEvent(self, event):
         event.accept()
