b0efc291c755d9228a274dee1715148cf06599fd
= <=>
2026-05-27 11:36:23 +0900

    終了

--- 2026/05/20260527_python_ros2_Service-Launch-Practice/ws/install/mode_switch_practice/share/mode_switch_practice/package.ps1
+++ 2026/05/20260527_python_ros2_Service-Launch-Practice/ws/install/mode_switch_practice/share/mode_switch_practice/package.ps1
@@ -111,6 +111,7 @@ function colcon_package_source_powershell_script {
 $env:COLCON_CURRENT_PREFIX=(Get-Item $PSCommandPath).Directory.Parent.Parent.FullName
 
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX/share/mode_switch_practice/hook/pythonpath.ps1"
+colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX/share/mode_switch_practice/hook/ament_prefix_path.ps1"
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX/../../build/mode_switch_practice/share/mode_switch_practice/hook/pythonpath_develop.ps1"
 
 Remove-Item Env:\COLCON_CURRENT_PREFIX
